7a144e87601189125b7485c88c68b328f523d914
Demétrio Alves Atra <datra@verg1759.lobtec.com.br>
2022-07-06 15:23:39 -0300

    v 1.1.0

--- src/App.js
+++ src/App.js
@@ -1,53 +1,54 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
 
   constructor (props) {
+  
     super(props)
     this.state = {
-      text: ''
+      txt: ''
     }
+
+    this.showMessage = this.showMessage.bind(this)
 
     this.phrases = [
       'Siga os bons e aprenda com eles.',
-      'O bom-senso vale mais do que muito conhecimento.', 
-      'O riso é a menor distância entre duas pessoas.', 
+      'O bom-senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
       'Deixe de lado as preocupações e seja feliz.',
       'Realize o óbvio, pense no improvável e conquiste o impossível.',
       'Acredite em milagres, mas não dependa deles.',
       'A maior barreira para o sucesso é o medo do fracasso.'
     ]
-
-    this.showMessage = this.showMessage.bind(this)
   }
 
-  showMessage() {
+  showMessage () {
     let state = this.state
     let number = Math.floor(Math.random() * this.phrases.length)
-    state.text = this.phrases[number]
+    state.txt = this.phrases[number]
     this.setState(state)
   }
 
   render () {
     return (
-      <div className='container'>
-        <img className='img' src={ require('./imgs/cookie.png') }/>
-        <Button name="Open" action={ this.showMessage }/>
-        <h3 className='text'>{ this.showMessage }</h3>
+      <div className = 'container'>
+        <img className = 'img' src = { require('./imgs/cookie.png') } />
+        <Btn nm = 'Open' act = { this.showMessage } />
+        <h3 className = 'text'>{ this.state.txt }</h3>
       </div>
     )
   }
 }
 
-class Button extends Component {
-  render() {
+class Btn extends Component {
+  render () {
     return (
       <div>
-        <button>{ this.props.name }</button>
+        <button onClick = { this.props.act }>{ this.props.nm }</button>
       </div>
     )
   }
 }
 
-export default App;
+export default App
